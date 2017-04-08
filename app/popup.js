ff618e1265f12a57e55d44549f3a6c6c1cb272d2
Lau Kondrup <lau.kondrup@gmail.com>
2017-04-09 00:00:59 +0200

    Misc cleaning and tidying up

--- app/popup.js
+++ app/popup.js
@@ -27,8 +27,6 @@ function sortByUrl(){
             tab.url = getDomain(tab.url);
         });
 
-        print(tabs);
-
         tabs.sort(function(a, b){
             if(a.url > b.url){
                 return 1;
@@ -62,7 +60,7 @@ function sortByUrl(){
 // }
 
 function getDomain(url){
-    // TODO A bit ugly code
+    
     var splitUrl = url.split('//');
     if(splitUrl.length === 2){
         url = splitUrl[1];
@@ -76,20 +74,17 @@ function getDomain(url){
     return url;
 }
 
-function print(input){
+function printJson(input){
     console.log(JSON.stringify(input, null, 2));
 }
 
 function mergeWindows(){
-    // TODO ugly code
     chrome.tabs.query({active: true}, function(activeTabs){
 
         chrome.tabs.query({currentWindow: false, pinned: false}, function(tabs){
-            tabIds = tabs.map(tab => tab.id);
+            tabIds = tabs.map(function(tab){ return tab.id; });
             chrome.tabs.move(tabIds, {windowId: activeTabs[0].windowId, index: -1});
         });
 
     });
-
-    
 }
