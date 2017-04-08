96d844e4c2222cb840297a12829dc19dc46a0220
Lau Kondrup <Lau Kondrup>
2017-04-08 20:42:28 +0200

    Sort by age

--- app/popup.js
+++ app/popup.js
@@ -1,24 +1,38 @@
-document.getElementById('sortAlphabetically').onclick = sortAlphabetically;
+document.getElementById('sortByAge').onclick = sortByAge;
 
-function sortAlphabetically(){
-    console.log("Hello World");
+function sortByAge() {
+    chrome.tabs.query({currentWindow: true,pinned: false},
+        function (tabs) {
+            var startIndex = tabs[0].id;
 
-    // chrome.tabs.query({
-    //     currentWindow: true,
-    //     pinned: true
-    // }, function(tabs) {
-    //     // Only move the tab if at least one tab is pinned
-    //     if (tabs.length > 0) {
-    //         var lastTab = tabs[ tabs.length - 1 ];
-    //         var tabIndex = lastTab.index + 1;
-    //         for (var i=0; i<tabs.length; ++i) {
-    //             if (tabs[i].id == tabId) {
-    //                 // Current tab is pinned, so decrement the tabIndex by one.
-    //                 --tabIndex;
-    //                 break;
-    //             }
-    //         }
-    //         chrome.tabs.move(tabId, {index: tabIndex});
-    //     }
-    // }); // End of chrome.tabs.query
+            var mappedTabs = tabs.map(function(tab){
+                return tab.id;
+            });
+
+            console.log(tabs);
+            console.log(mappedTabs);
+
+            tabs.sort(function(a, b){
+                return a.id - b.id;
+            });
+
+            // tabs.sort(function(a, b){
+            //     if(a.id > b.id){
+            //         return 1;
+            //     }
+            //     if(a.id < b.id){
+            //         return -1;
+            //     }
+            //     return 0;
+            // });
+
+            mappedTabs = tabs.map(function(tab){
+                return tab.id;
+            });
+            console.log('after sort:');
+            console.log(mappedTabs);
+
+            chrome.tabs.move(mappedTabs, {index: -1});
+        }
+    ); 
 }
