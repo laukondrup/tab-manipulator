804872837484104e19a44327bf53d8555860fa9c
Lau Kondrup <lau.kondrup@gmail.com>
2017-06-07 20:41:37 +0200

    style: popup.js

--- popup.js
+++ popup.js
@@ -5,29 +5,27 @@
 
 // chrome.tabs.create({url: 'popup.html'});
 
-function sendMessage(message){
-    chrome.runtime.sendMessage({action: message}, function(response){
-        console.log('MessageResponse: ', response);
-    });
+function sendMessage(message) {
+  chrome.runtime.sendMessage({ action: message }, (response) => {
+    console.log('MessageResponse: ', response);
+  });
 }
 
-// TODO: encapsulate
 const actionElements = document.getElementsByClassName('action');
-for (const key in actionElements){
-    const action = actionElements[key];
-    action.onclick = () => sendMessage(action.id);
+
+for (const key in actionElements) {
+  const action = actionElements[key];
+  action.onclick = () => sendMessage(action.id);
 }
 
-window.onkeyup = function(e){
-    if(e.shiftKey || e.ctrlKey){ return; } 
-        
-    if(e.key === "a"){
-        sendMessage('sortByAge');
-    }
-    else if(e.key === "u"){
-        sendMessage('sortByUrl');
-    }
-    else if(e.key === "d"){
-        sendMessage('sortByNumDomain');
-    }
+window.onkeyup = function (e) {
+  if (e.shiftKey || e.ctrlKey) { return; }
+
+  if (e.key === 'a') {
+    sendMessage('sortByAge');
+  } else if (e.key === 'u') {
+    sendMessage('sortByUrl');
+  } else if (e.key === 'd') {
+    sendMessage('sortByNumDomain');
+  }
 };
